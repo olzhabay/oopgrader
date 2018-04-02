41211b8af95766102b2603410c53f5adee43ec99
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2018-04-02 14:16:03 +0900

    upgraded

---
+++ README.md
@@ -0,0 +1,27 @@
+# Student Assignment Grader
+
+Script automatically retrieves students projects from their gitlab accounts that have been forked from original repository and grade them.
+
+### Before using
+Get user's private token https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html
+
+Get project id, which is in the settings of the project
+
+
+### How to use?
+* Create new directory for semester, like 2018s, and place the script inside of directory.
+* Place inside of directory `testcases` assignment's testcases with Makefile, input/output and json file.
+* Input and output testcases must look like `input|number|.txt`, like **input1.txt** and **output1.txt**
+* Json file must have same name as directory and have following setup:
+```json
+{
+  "name" : "assign",                      # name of assignment
+  "driver" : "False",                     # True if driver assignment, False if stdin
+  "valgrind" : [ "False", "0" ],          # True if to use Valgrind, and driver number to run Valgrind
+  "number" : "5",                         # Number of testcases or drivers
+  "timelimit" : "5"                       # Time limit to run each testcase in sec
+  "due_data" : "2018-03-30T20:00:00.000"  # Due date %Y-%m-%dT%H:%M:%S.%f
+}
+```
+* Run script with assignment name as argument, as `python script.py assign1`
+* Results will be saved in same directory as script, in *.out file with assignment name (ex. assign1.out)
--- script.py
+++ script.py
@@ -1,26 +1,14 @@
 import os
-import json
-import glob
-import sys
-import difflib
 import argparse
-import pygit2
 import subprocess
-from pygit2 import Repository
+import json
+import urllib2
+import csv
+import time
+import datetime
 from threading import Timer
 
 
-class GitCallbacks(pygit2.RemoteCallbacks):
-    def credentials(self, url, username_from_url, allowed_types):
-        if allowed_types == pygit2.GIT_CREDTYPE_USERNAME:
-            return pygit2.Username("git")
-        elif allowed_types == pygit2.GIT_CREDTYPE_SSH_KEY:
-            return pygit2.Keypair("git", "id_rsa.pub", "id_rsa", "")
-        else:
-            return None
-
-
-# return without blank space ending
 def clean_ending(file):
     while file[-1] == '\n' or file[-1] == '' or file[-1] == ' ':
         file.pop(-1)
@@ -37,7 +25,6 @@ def compare_output(output, answer):
             diff = diff + 1
     if len(ans_lines) < len(out_lines):
         diff = diff + (len(out_lines) - len(ans_lines))
-    print diff
     return diff
 
 
@@ -48,98 +35,133 @@ def run(cmd, timeout_sec):
     try:
         timer.start()
         stdout, stderr = proc.communicate()
-        return stderr, stdout
+        if stderr == "":
+            return 0, stdout
+        else:
+            return 1, stdout
     finally:
         timer.cancel()
-        return 1, "timeout"
 
 
 # parse argument
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", help="set assignment name (ex. assign1)", type=str, required=True)
+parser.add_argument("--repo_id", help="set repository id", type=str, required=True)
 args = parser.parse_args()
 if not args.name:
     print("set assignment name")
     exit(1)
+if not args.repo_id:
+    print("set repository id")
+    exit(1)
 
-# set vars
+# set directory vars
 project_name = args.name
+root_repo_id = args.repo_id
 current_dir = os.getcwd()
-submission_dir = "%s/submissions" % current_dir
-testcases_dir = "%s/testcases" % current_dir
+graded_dir = "%s/graded/%s" % (current_dir, project_name)
+testcases_dir = "%s/testcases/%s" % (current_dir, project_name)
+# gitlab token
+private_token = "7ZrthQvAHozC4sDaVs2e"
 
-# create dir
-os.system("mkdir %s" % submission_dir)
+url = "https://gitlab.com/api/v4/projects/%s/forks?private_token=%s&per_page=100" % (root_repo_id, private_token)
+data = json.load(urllib2.urlopen(url))
 
-student_list = [stud.rstrip() for stud in open("registration.txt")]
-student_results = []
+# read configuration file
+conf_file_name = "%s/%s.json" % (testcases_dir, project_name)
+configuration = json.load(open(conf_file_name))
+if project_name != configuration["name"]:
+    print("Configuration file is wrong")
+    exit(1)
 
-for student in student_list:
-    grade = 0
-    student_dir = "%s/%s" % (submission_dir, student)
-    student_project_dir = "%s/%s" % (student_dir, project_name)
-    os.system("mkdir %s" % student_dir)
-    git_url = "ssh://gitlab.com/%s/%s" % (student, project_name)
+driver = configuration["driver"] == 'True'
+test_number = int(configuration["test_number"])
+valgrind = configuration["valgrind"][0] == 'True'
+valgrind_test = int(configuration["valgrind"][1])
+timelimit = configuration["timelimit"]
+due_date = configuration["due_date"]
+test_grade = (100 / test_number if not valgrind else 100 / (test_number + 1))
 
-    # clone repository if exists
-    try:
-        student_repo = pygit2.clone_repository(git_url, student_dir, callbacks=GitCallbacks)
-    except:
-        student_results.insert([student, grade, ["repo absent"]])
-        continue
+# all results
+students_results = []
+
+for meta in data:
+    # setup git vars
+    visibility = (1 if meta['visibility'] == "private" else 0)
+    student_id = meta['owner']['username']
+    student_id = student_id.replace("cs", "")
+    last_activity = meta['last_activity_at']
+    path = meta['path_with_namespace']
+    student_git_url = "https://oauth2:%s@gitlab.com/%s.git" % (private_token, path)
+    student_project_dir = "%s/%s" % (graded_dir, student_id)
+
+    # setup grading vars
+    student_total_grade = 0.0
+    student_testcase_grade = 0.0
+    student_comment = ''
+
+    # clone
+    os.system("git clone %s %s/." % (student_git_url, student_project_dir))
 
     # copy testcases and makefile
-    os.system("cp %s/%s/* %s/." % (testcases_dir, project_name, student_project_dir))
+    os.system("cp %s/* %s/." % (testcases_dir, student_project_dir))
 
     # run makefile
-    (stat, output) = run("make -C %s" % student_project_dir, 30)
+    (stat, output) = run(["make", "-C" "%s/" % student_project_dir], 60)
     if stat != 0:
-        student_results.insert([student, grade, ["does not compile"]])
-        continue
-
-    # read configuration file
-    conf_file_name = "%s/%s.json" % (student_project_dir, project_name)
-    configuration = json.load(open(conf_file_name))
-    if project_name != configuration["name"]:
-        print("Configuration file is wrong")
-        exit(1)
-    driver = configuration["driver"]
-    number = configuration["number"]
-    valgrind = configuration["valgrind"][0]
-    valgrind_test = configuration["valgrind"][1]
-    timelimit = configuration["timelimit"]
-    test_grade = 0
-    if valgrind:
-        test_grade = 100 / (number + valgrind)
+        student_comment += "| does not compile "
     else:
-        test_grade = 100 / number
-
-    # start testing
-    result_comment = ["test results: "]
-    for i in range(0, number):
-        if driver:
-            (stat, output) = run("%s/driver%d" % (student_project_dir, i), timelimit)
-        else:
-            (stat, output) = run("%s/%s < input%d.txt" % (student_project_dir, project_name, i), timelimit)
-        if stat == 0:
-            diff = compare_output(output, open("%s/output%d.txt" % (student_project_dir, i), 'r'))
-            if diff == 0:
-                grade += test_grade
-                result_comment.append("test%d")
+        # adding 5 points for compilable code
+        student_total_grade += 5
+        # start testing
+        for i in range(0, test_number):
+            if driver:
+                (stat, output) = run(["%s/driver%d" % (student_project_dir, i)], timelimit)
             else:
-                result_comment.append("test%d-diff(%d)" % (i, diff))
-        else:
-            result_comment.append("test%d-error(%s)" % (i, output))
+                (stat, output) = run(["%s/%s" % (student_project_dir, project_name), "<", "input%d.txt" % i], timelimit)
+            if stat == 0:
+                diff = compare_output(output, open("%s/output%d.txt" % (student_project_dir, i), 'r'))
+                if diff == 0:
+                    student_testcase_grade += test_grade
+                    student_comment += "| test%d-success " % i
+                else:
+                    student_comment += "| test%d-diff(%d) " % (i, diff)
+            else:
+                student_comment += "| test%d-error(%s) " % (i, output)
 
-    # test memory leak
-    if valgrind:
-        (stat, output) = run("valgrind -v %s/driver%d" % (student_project_dir, valgrind_test), timelimit)
-        if 'no leak' in output:
-            test_grade += test_grade
-            result_comment.append("no leak")
+        # test memory leak
+        if valgrind:
+            (stat, output) = run(["valgrind", "-v", "%s/driver%d" % (student_project_dir, valgrind_test)], timelimit)
+            if 'no leak' in output:
+                student_testcase_grade += test_grade
+                student_comment += "| no leak "
+            else:
+                student_comment += "| memory leak "
+
+    # checking if code is not empty, giving 5 points
+    code_lines = sum(1 for line in open('%s/%s.cpp' %(student_project_dir, project_name)))
+    if code_lines > 15:
+        student_total_grade += 5
+    # due date calc
+    due_date_penalty = 0
+    due_date_dt = datetime.strptime(due_date, '%Y-%m-%dT%H:%M:%S.%f')
+    student_date_dt = datetime.struct_time(last_activity, '%Y-%m-%dT%H:%M:%S.%fZ')
+    delta = student_date_dt - due_date_dt
+    if delta.days > 0 or delta.seconds > 0:
+        if delta.days >= 3:
+            due_date_penalty = 10
+            student_comment += "| due date limit "
+        else:
+            due_date_penalty = 1 + delta.days
+            student_comment += "| %d days due" % due_date_penalty
+
+    # calc total grade
+    student_total_grade += student_testcase_grade * 0.9
+    student_total_grade = (1 - due_date * 0.1) * student_total_grade
 
     # add student's result to the list
-    student_results.append([student, grade, result_comment])
+    students_results.append([student_id, round(student_total_grade, 1), student_comment])
 
-# print result
-print student_results
+with open('%s/%s_grade.txt' % (graded_dir, project_name), 'wb') as out_file:
+    wr = csv.writer(out_file, quoting=csv.QUOTE_ALL)
+    wr.writerow(students_results)
